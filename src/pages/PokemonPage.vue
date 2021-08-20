ad018a4f1b6e2e6eab351235fc599a19a81b5cbb
mmiguez77 <martinmiguez77@gmail.com>
2021-08-20 02:32:24 -0300

    first Commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>¿Quién es este Pokemon?</h1>
+
+    <PokemonPicture/>
+    <PokemonOptions/>
+
+</template>
+
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+export default {
+  components: {
+    PokemonPicture,
+    PokemonOptions
+  },
+};
+</script>
+
